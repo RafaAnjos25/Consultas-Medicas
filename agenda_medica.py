3a9154447454bca10c63b09764118fd2d94c8b5e
RafaAnjos25 <rafaelaugusto.0112@gmail.com>
2025-05-31 14:11:56 -0300

    Finalizando green tests

--- agenda_medica.py
+++ agenda_medica.py
@@ -1,11 +1,3 @@
-
-
-agenda = {
-            '1': {'Nome': 'Joao', 'Medico': 'Rafael', 'Horario': '2025-06-02'},
-            '2': {'Nome': 'Mari', 'Medico': 'Rafael', 'Horario': '2025-06-07'}
-        }    
-
-
 class EmailService:
     def send_email(self, recipient, message):
         return recipient is not None and message is not None
@@ -17,55 +9,49 @@ class Notification:
     def notify_pacient(self, email, message):
         return self.email_service.send_email(email, message)
     
-class consulta:
-    def agendar_consulta(self, id_consulta, paciente, medic, time):
-        self.id_consulta = id_consulta
-        self.paciente = paciente
-        self.medic = medic
-        self.time = time
-        global agenda
-
-        agenda = {
-        self.id_consulta : {'Nome': self.paciente, 'Medico': self.medic, 'Horario': self.time}
-        }
-        return agenda
-
-    def cancelar_consulta(self,id_consulta):
-        global agenda
-        self.id_consulta = id_consulta
-
-        del agenda[self.id_consulta]
     
 class medic:
-    def filtrar_especialiade(self, filtro, filtragem):
+    def filtrar_especialiade(self, filtro):
         medicos = {
             '1': {'Nome': 'Joao', 'Especialidade': 'Ortopedista'},
-            '2': {'Nome': 'Mara', 'Especialidade': 'Ortopedista'}
+            '2': {'Nome': 'Mara', 'Especialidade': 'Ortopedista'},
+            '3': {'Nome': 'Jose', 'Especialidade': 'Pediatra'},
+            '4': {'Nome': 'Rafael', 'Especialidade': 'Oftamologista'}
         }
-
-        self.filtro = filtro
 
         filtragem = {
             id : detalhes    
             for id, detalhes in medicos.items()
-                if detalhes['Especialidade']  == self.filtro
+                if detalhes['Especialidade']  == filtro
         }
-        print(json.dumps(agenda, indent=3, ensure_ascii=False))
+
+        for id, detalhes in filtragem.items():
+            print(f"{id}: {detalhes}")
         return filtragem
 
+agenda_consulta = {
+    '1': {'Nome': 'Joao', 'Medico': 'Rafael', 'Horario': '2025-06-02, 15:30'},
+    '2': {'Nome': 'Mari', 'Medico': 'Rafael', 'Horario': '2025-06-07, 16:50'}
+    }  
+
 class agenda:        
-    def return_agenda():
-            agenda1 = {
-                    '1': {'Nome': 'Joao', 'Medico': 'Rafael', 'Horario': '2025-06-02'},
-                    '2': {'Nome': 'Mari', 'Medico': 'Rafael', 'Horario': '2025-06-07'}
-                }    
-            return agenda1
+    
+    def agendar_consulta(self, id_consulta, paciente, medic, time):
+        global agenda_consulta
+
+        agenda_consulta [id_consulta] = {'Nome': paciente, 'Medico': medic, 'Horario': time}
+    
+        return agenda_consulta
+    
+    def cancelar_consulta(self,id_consulta):
+        global agenda_consulta
+        
+        consulta_removida = agenda_consulta.pop(id_consulta)
+        return consulta_removida
 
     def vericar_agenda(self):
-            valor = agenda.return_agenda()
-
-            print(valor)
+        global agenda_consulta  
             
-            for id_chave, detalhes in valor.items():
-                print(f"{id_chave}: {detalhes}")
-            return valor
+        for id_chave, detalhes in agenda_consulta.items():
+            print(f"{id_chave}: {detalhes}")
+        return agenda_consulta
--- test_codigos.py
+++ test_codigos.py
@@ -1,6 +1,5 @@
 import unittest
 from agenda_medica import Notification
-from agenda_medica import consulta
 from agenda_medica import medic
 from agenda_medica import agenda
 from unittest.mock import MagicMock
@@ -10,18 +9,53 @@ class test_agenda_medica(unittest.TestCase):
     def setUp(self):
         self.mock = MagicMock()
         self.notification = Notification(self.mock)
-        self.consulta = consulta()
         self.medic = medic()
         self.agenda = agenda()
 
     def tearDown(self):
         del self.mock
         del self.notification
-        del self.consulta
+        del self.agenda
         del self.medic
+
+    def test_notification_sucess(self):
+        email = "guts@gmai.com"
+        message = "Dia da consulta"
+
+        self.mock.send_email.return_value = True
+        self.assertTrue(self.notification.notify_pacient(email, message))
+
+        self.mock.send_email.assert_called_once_with(email, message)
+
+    def test_notification_fail(self):
+        email = None
+        message = None
+
+        self.mock.send_email.return_value = False
+        self.assertFalse(self.notification.notify_pacient(email, message))
+
+        self.mock.send_email.assert_called_once_with(email, message)
+
+    def test_agendar_consulta_sucess(self):
+        id_consulta = '3'
+        pacient = "Eduardo"
+        medic = "Rafael"
+        time = "2025-07-02, 13:18"
+
+        self.assertTrue(self.agenda.agendar_consulta(id_consulta, pacient, medic, time))
+
+    def test_cancelar_consulta_sucess(self):
+        id_consulta = '1'
+
+        self.assertTrue(self.agenda.cancelar_consulta(id_consulta))
 
     def test_verificar_agenda(self):
         self.assertTrue(self.agenda.vericar_agenda())
+
+    def test_filtrar_especialidade(self):
+        filtro = 'Oftamologista'
+
+        self.assertTrue(self.medic.filtrar_especialiade(filtro))
 
 if __name__ == "__main__":
     unittest.main()
